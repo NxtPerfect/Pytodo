7917e4f65b834c64272d3e18fc9f5e132a41df37
NxtPerfect <alakaxan@protonmail.com>
2022-11-25 15:57:05 +0100

    Final commit

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# Make .txt file with all the todos
-# add id based on the previous one, else if no other id, id = 0
-# Simple functions that do one thing
-# Id is the line the task is in the text file
-# Should the tasks be just removed when finished, or marked as finished
-
-
 class Task:
     def __init__(self, is_completed: bool, title: str, description: str):
         try:
@@ -17,27 +10,29 @@ class Task:
 
 
 if __name__ == "__main__":
-    tasks_list = []
-    task_id = 0
+    TASKS_LIST = []
+    TASK_ID = 0
+    COMPLETED_TASKS = 0
     while True:
         print(
-            f"Welcome to PyToDo\nHere's your task list:{tasks_list}")
+            f"Welcome to PyToDo\nHere's your task list:{TASKS_LIST}")
+        print(f"Today you managed to complete {COMPLETED_TASKS} tasks")
         print("What would you like to do?")
         print("1. Add new task")
         print("2. Complete existing task")
-        print("3. Remove completed tasks")
         user_input = input()
         match user_input:
             case '1':
                 new_title = input("What's the task title?\t")
                 new_description = input("What's the task description?\t")
                 new_task = Task(False, new_title, new_description)
-                task_id = task_id + 1
-                tasks_list.append(new_task)
+                TASK_ID = TASK_ID + 1
+                TASKS_LIST.append(new_task)
             case '2':
                 search_id = input("What's the task number?")
                 try:
                     search_id = int(search_id)
-                    tasks_list.pop(int(search_id))
+                    TASKS_LIST[search_id].is_completed = True
+                    COMPLETED_TASKS = COMPLETED_TASKS + 1
                 except ValueError:
-                    print("Search id isn't a valid number")
+                    print("This task doesn't exist")
